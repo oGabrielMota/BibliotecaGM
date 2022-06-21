b36ac33e7accf957a5a882b1d6e9b604fedc8874
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-06-21 19:10:02 -0300

    Adicionando documentação via Swagger

--- BibliotecaGM/src/main/java/br/com/aceleragep/BibliotecaGM/BibliotecaGmApplication.java
+++ BibliotecaGM/src/main/java/br/com/aceleragep/BibliotecaGM/BibliotecaGmApplication.java
@@ -3,6 +3,8 @@ package br.com.aceleragep.BibliotecaGM;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BibliotecaGmApplication {
 
